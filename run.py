be80333c32963386e7c9c27cfea806183489ccb6
ZHOU BIN <zhoubinxyz@gmail.com>
2022-07-07 13:42:15 +0800

    .

--- run.py
+++ run.py
@@ -1,14 +1,29 @@
 import os
 from itertools import product
+from src.ae.trainer import Trainer
+from src.ae.config import Config
 
 loss_fns = ['mse', 'ce', 'normal']
 models = ['unet']
 
 for loss_fn, model in product(loss_fns, models):
-    os.system(f"python main.py --model {model} --loss_fn {loss_fn} --task train")
-    os.system(f"python main.py --model {model} --loss_fn {loss_fn} --task decode --load_ckpt "
-              f"ckpt/ae_{loss_fn}_train_cifar10_{model}/best.pth.tar")
-    os.system(f"python main.py --model {model} --loss_fn {loss_fn} --task probing --load_ckpt "
-              f"ckpt/ae_{loss_fn}_train_cifar10_{model}/best.pth.tar")
-    os.system(f"python main.py --model {model} --loss_fn {loss_fn} --task tsne --load_ckpt "
-              f"ckpt/ae_{loss_fn}_train_cifar10_{model}/best.pth.tar")
+    cfg = Config()
+    cfg.model = model
+    cfg.loss_fn = loss_fn
+    cfg.load_ckpt = f"ckpt/ae_{loss_fn}_train_cifar10_{model}/best.pth.tar"
+
+    cfg.task = "train"
+    trainer = Trainer(**cfg.__dict__)
+    trainer.run()
+
+    cfg.task = "decode"
+    trainer = Trainer(**cfg.__dict__)
+    trainer.run()
+
+    cfg.task = "probing"
+    trainer = Trainer(**cfg.__dict__)
+    trainer.run()
+
+    cfg.task = "tsne"
+    trainer = Trainer(**cfg.__dict__)
+    trainer.run()
